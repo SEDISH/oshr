832ce149f750bff87c9727ad12add95d696bacc0
wstrzelczyk <wstrzelczyk@soldevelo.com>
2017-04-11 08:17:41 +0200

    #59 Fixed build image

--- run.sh
+++ run.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 set -e
+
 sleep 1
 echo "### Init database for openmrs ###"
 mysql -u root -pshr -e "DROP DATABASE openmrs;"
+
 echo "Delete exist database openmrs"
 mysql -u root -pshr -e "CREATE DATABASE openmrs;"
+
 echo "Create database openmrs from file < openmrs.sql"
 mysql -u root -pshr openmrs < openmrs.sql
+
 echo "Finish init database"
 sleep 5
+
 echo "### Start TOMCAT ###"
 catalina.sh run
