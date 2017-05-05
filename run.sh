4a74798abd34d45ea6842800ff9cd070f780e52d
przemyslawStudzinskiSolDevelo <pstudzinski@soldevelo.com>
2017-05-05 12:57:38 +0200

    #93: Added OpenXDS configuration

--- run.sh
+++ run.sh
@@ -7,8 +7,6 @@ USERADD=/usr/sbin/useradd
 ADDGROUP=/usr/sbin/addgroup
 ADDUSER=/usr/sbin/adduser
 
-#service openshr stop || true
-
 if ! getent group $USERNAME >/dev/null; then
     	echo "Creating group $USERNAME"
     	$ADDGROUP --quiet --system $USERNAME
@@ -18,23 +16,21 @@ fi
 if id -u $USERNAME >/dev/null 2>&1; then
     	echo "System user $USERNAME exists."
 else
-    	echo "System user $USERNAME does not exist. adding."
+    	echo "System user $USERNAME does not exist. Adding."
     	$USERADD  $USERNAME -g $USERNAME -M -s /bin/bash 
 
 	sleep 1
 
-	# Copy base/dependency modules to module folder
+    	# Copy base/dependency modules to module folder
 	echo "Copying module dependencies and reference application modules..."
 	mkdir -pv $OPENMRS_MODULES
 	cp /root/temp/modules/*.omod $OPENMRS_MODULES
 	echo "Modules copied."
-	export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms512m -Xmx1024m -DOPENMRS_APPLICATION_DATA_DIRECTORY=/usr/share/openshr/openmrs"
 
-
-	echo "### Init database for openmrs ###"
+	echo "### Init database for OpenMRS ###"
 	mysql -u root -pshr -e "DROP DATABASE openmrs;"
 
-	echo "Delete exist database openmrs"
+	echo "Delete exist database OpenMRS"
 	mysql -u root -pshr -e "CREATE DATABASE openmrs;"
 
 	echo "Create database openmrs from file < openmrs.sql"
