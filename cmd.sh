5717da990f67fe723f04a114f1d752e3b465f02b
Arkadiusz Lalo <alalo@soldevelo.com>
2018-05-30 16:15:27 +0200

    Fixed the openSHR initial script

--- cmd.sh
+++ cmd.sh
@@ -19,7 +19,7 @@ if id -u $USERNAME >/dev/null 2>&1; then
     	echo "System user $USERNAME exists."
 else
     	echo "System user $USERNAME does not exist. Adding."
-    	$USERADD  $USERNAME -g $USERNAME -M -s /bin/bash 
+    	$USERADD  $USERNAME -g $USERNAME -M -s /bin/bash
 
 	sleep 1
 
@@ -27,34 +27,34 @@ else
 	echo "Copying module dependencies and reference application modules..."
 	mkdir -pv $OPENMRS_MODULES
 	cp /root/temp/modules/*.omod $OPENMRS_MODULES
-	echo "Modules copied."	
+	echo "Modules copied."
 
-	DB=`mysql -u root -pshr --skip-column-names -e "SHOW DATABASES LIKE '${OPENMRS_DATABASE}'"`
+	DB=`mysql -u root -p${MYSQL_ROOT_PASSWORD} --skip-column-names -e "SHOW DATABASES LIKE '${OPENMRS_DATABASE}'"`
 	if [ "$DB" != "${OPENMRS_DATABASE}" ]; then
 		echo "# Init database for OpenMRS #"
 
-		mysql -u root -pshr -e "CREATE DATABASE ${OPENMRS_DATABASE};"
+		mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE ${OPENMRS_DATABASE};"
 
-		mysql -u root -pshr -e "GRANT ALL ON openmrs.* to '${MYSQL_USER}'@'%' identified by '${MYSQL_PASSWORD}';"
+		mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON openmrs.* to '${MYSQL_USER}'@'%' identified by '${MYSQL_PASSWORD}';"
 
 		echo "Create database openmrs from file < openmrs.sql"
-		mysql -u root -pshr openmrs < openmrs.sql
+		mysql -u root -p${MYSQL_ROOT_PASSWORD} openmrs < openmrs.sql
 
 		echo "Load concepts to openmrs database from file < concepts.sql"
-		mysql -u root -pshr openmrs < concepts.sql
+		mysql -u root -p${MYSQL_ROOT_PASSWORD} openmrs < concepts.sql
 
 		if [ $data_file -eq 1 ]; then
 			echo "Load data to openmrs database from file < shrdata.sql"
-			mysql -u root -pshr openmrs < shrdata.sql
+			mysql -u root -p${MYSQL_ROOT_PASSWORD} openmrs < shrdata.sql
 		else
 			echo "Add shrdata.sql file to this direcatory to load data into SHR database"
 		fi
 
 		echo "Update database openmrs with SHR configuration from file < openshr-configuration.sql"
-		mysql -u root -pshr openmrs < openshr-configuration.sql
+		mysql -u root -p${MYSQL_ROOT_PASSWORD} openmrs < openshr-configuration.sql
 
 		echo "Finish init database"
-		rm openmrs.sql	
+		rm openmrs.sql
 		rm openshr-configuration.sql
 	else
 		echo "# Database already exists. Skipping database creation and import. #"
